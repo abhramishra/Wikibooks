3b9138203fb53dfa390dc04bfc06f48baacdfde8
abhramishra <abhramishra1995@gmail.com>
2024-12-10 21:41:05 +0530

    Added style

--- src/component/BookList.jsx
+++ src/component/BookList.jsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
 import BookShow from "./BookShow";
+
 function BookList({books, onEdit, onDelete}) {
     return (
         <div>
             <h2>Reading List - {books.length}</h2>
+            <div>
             {
                 books.map(book => {
                     return <BookShow key={book.id} book={book} onEdit={onEdit} onDelete={onDelete} />
                 })
             }
+            </div>
         </div>
     )
 }
